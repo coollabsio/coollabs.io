27659aeba2f0c5c57e2df4bb2a07ee86b2e63b4d
Andras Bacsai <andras.bacsai@gmail.com>
2025-02-02 16:03:57 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://coolify.io",
+  site: "https://coollabs.io",
   vite: {
     optimizeDeps: {
       include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
